25d722ebcceca797c97a1c8eac360f0bda9f6954
rsgc-peterson-e <epeterson@rsgc.on.ca>
2018-05-27 18:14:36 -0500

    cleaned up code and fixed swapped ones tens on score displays

--- Long ISP/main.asm
+++ Long ISP/main.asm
@@ -7,16 +7,11 @@
 
 .cseg                                          ;load into Program Memory
 #include "prescalers.h"
-//#include "doubleDabble.s" // move double dabble algorithms and other macros here at some point
 .org   0x0000                          ;start of Interrupt Vector (Jump) Table
         rjmp    reset                           ;address  of start of code
 .equ size = 15
-startTable: //.byte   
-		// A set of random numbers until I know how to generate them in assembly language
-		.DB 4, 14, 32, 94, 28, 69, 48, 51, 15, 0
 .org 0x000E
 	rjmp TIM2_COMPA
-endTable:
 .org 0x002A
 	rjmp ADC_Complete
 .org 0x0020
@@ -28,11 +23,6 @@ endTable:
     out             spl,r16                 ; setting the Stack Pointer to
     ldi             r16, high(RAMEND)       ; the end of SRAM to support
     out             sph,r16                 ; function calls, etc.
-
-   //ldi             xl,low(startTable<<1)   ; position X and Y pointers to the
-   //ldi             xh,high(startTable<<1)  ; start and end addresses of
-   //ldi             yl,low(endTable<<1)     ; our data table, respectively
-   //ldi             yh,high(endTable<<1)    ;
    movw    z,x
 
 .def original = r16 ; value to be pushed through double dabble algorithm
@@ -63,7 +53,6 @@ timers:
 
 	ldi r16, 1 << OCIE2A ; set timer interrupt enable bit
 	sts TIMSK2, r16 ; enable the interrupt
-	//sei ; global interrupt enable
 	ret
 
 ADCInit:
@@ -83,8 +72,6 @@ dummy:
 
 ADC_Complete:
 	lds newValue, ADCH ; grab ADC reading and place in gp reg
-	
-	
 	reti
 
 T0Init: ; initialize T0 interrupt to schedule ADC conversions
@@ -129,12 +116,10 @@ random:
 
 start:
 	out 0x0A, hundreds ; clear DDRD register using 0 value in hundreds reg
-	//mov zl, index
 
 	mov r17, newValue ; copy the seed
 	and r17, r18 
 	ldi r18, 0x43
-	//clr r19 
 	; generate random # in newValue reg using ADC reading as seed... reference: https://www.avrfreaks.net/forum/i-need-pseudo-random-number-generator-attiny-13
 	lsr r17
 	adc r18, r19
@@ -152,120 +137,57 @@ start:
 
 	mov original, newValue
 	mov copy, newValue
-
-	//movw z, x
-	//mov original, index
 	ret
 
 
-getInput:
-/*	in input, 0x09 ; load PIND into input register
-	lsr input ; fix off by one bit issue due to wiring config
-	andi input, 0b00001111 ; mask upper nibble off of input as the pins left floating cause eratic readings
-	
-    add zl, input ; add input value to low byte of z to set it to the correct index in the array containing the converted binary of the given graycode
-	lpm original, z ; load the corresponding binary value from table                             
-    movw z,x*/
-
-	// Get random byte value to be challenge val for binary challenge
-	//inc input
-
-	ret 
-	
-	
-/*doubleDabble:
-	ldi addReg, 3
-	ldi times, 8
-	clr onesTens
-	clr hundreds
-decideStep:
-; check if 3 needs to be added to any of the ones tens or hundreds nibbles
-checkOnes:
-	mov working, onesTens
-	andi working, 0b00001111 ; analyze only the lower nibble where ones value is
-	cpi working, 5
-	brsh addThreeOnes
-checkTens:
-	mov working, onesTens
-	andi working, 0b11110000 ; analyze only the upper nibble where tens value is stored
-	cpi working, 80
-	brsh addThreeTens
-checkHundreds:
-	cpi hundreds, 5
-	brsh addThreeHundreds
-; bit shift
-shift:
-	lsl original
-	rol onesTens
-	rol hundreds
-	dec times
-	cpi times, 0 ; if the original value has been shifted 8 times move to POV section of the code
-	breq endDabble //display
-	rjmp decideStep ; otherwise repeat
-
-addThreeOnes:
-	add onesTens, addReg
-	rjmp checkTens
-
-addThreeTens:
-	swap onesTens
-	add onesTens, addReg
-	swap onesTens
-	rjmp checkHundreds
-
-addThreeHundreds:
-	add hundreds, addReg
-	rjmp shift
-endDabble:
-	ret*/
-.MACRO shiftOut // MSBFIRST Shiftout
+.MACRO shiftOut ; MSBFIRST Shiftout
 	cbi PORTB, latch
-	// handle shifting data
+	; handle shifting data
 	cbi PORTB, clk
 	cbi PORTB, data
-	sbrc @0, 7 // skip if bit in register passed to macro is cleared
+	sbrc @0, 7 ; skip if bit in register passed to macro is cleared
 	sbi PORTB, data
 	sbi PORTB, clk
 
 	cbi PORTB, clk
 	cbi PORTB, data
-	sbrc @0, 6 // skip if bit in register passed to macro is cleared
+	sbrc @0, 6 ; skip if bit in register passed to macro is cleared
 	sbi PORTB, data
 	sbi PORTB, clk
 
 	cbi PORTB, clk
 	cbi PORTB, data
-	sbrc @0, 5 // skip if bit in register passed to macro is cleared
+	sbrc @0, 5 ; skip if bit in register passed to macro is cleared
 	sbi PORTB, data
 	sbi PORTB, clk
 
 	cbi PORTB, clk
 	cbi PORTB, data
-	sbrc @0, 4 // skip if bit in register passed to macro is cleared
+	sbrc @0, 4 ; skip if bit in register passed to macro is cleared
 	sbi PORTB, data
 	sbi PORTB, clk
 
 	cbi PORTB, clk
 	cbi PORTB, data
-	sbrc @0, 3 // skip if bit in register passed to macro is cleared
+	sbrc @0, 3 ; skip if bit in register passed to macro is cleared
 	sbi PORTB, data
 	sbi PORTB, clk
 
 	cbi PORTB, clk
 	cbi PORTB, data
-	sbrc @0, 2 // skip if bit in register passed to macro is cleared
+	sbrc @0, 2 ; skip if bit in register passed to macro is cleared
 	sbi PORTB, data
 	sbi PORTB, clk
 
 	cbi PORTB, clk
 	cbi PORTB, data
-	sbrc @0, 1 // skip if bit in register passed to macro is cleared
+	sbrc @0, 1 ; skip if bit in register passed to macro is cleared
 	sbi PORTB, data
 	sbi PORTB, clk
 
 	cbi PORTB, clk
 	cbi PORTB, data
-	sbrc @0, 0 // skip if bit in register passed to macro is cleared
+	sbrc @0, 0 ; skip if bit in register passed to macro is cleared
 	sbi PORTB, data
 	sbi PORTB, clk
 
@@ -273,7 +195,6 @@ endDabble:
 .ENDMACRO
 
 .MACRO doubleDabble
-//doubleDabble:
 	ldi addReg, 3
 	ldi times, 8
 	clr onesTens
@@ -300,7 +221,7 @@ shift:
 	rol hundreds
 	dec times
 	cpi times, 0 ; if the @0 value has been shifted 8 times move to POV section of the code
-	breq endDabble //display
+	breq endDabble
 	rjmp decideStep ; otherwise repeat
 
 addThreeOnes:
@@ -320,7 +241,7 @@ endDabble:
 	//ret
 .ENDMACRO
 
-.MACRO numDisplaySelect // MACRO to enable display of choice in number display
+.MACRO numDisplaySelect ; MACRO to enable display of choice in number display
 	cbi PORTB, PB2
 	cbi PORTB, PB1
 	cbi PORTB, PB0
@@ -328,7 +249,7 @@ endDabble:
 	sbi PORTB, @0
 .ENDMACRO
 
-.MACRO scoreDisplaySelect // MACRO to enable appropiate score display ones or tens
+.MACRO scoreDisplaySelect ; MACRO to enable appropiate score display ones or tens
 	cbi PORTC, PC4
 	cbi PORTC, PC5
 
@@ -357,9 +278,6 @@ endDabble:
 display:
 	rcall initNumDisplay ; set all transistor manipulation pins to output
 	numDisplaySelect PB2
-	//ldi r16, 0xFF
-	//out DDRC, r16 ; set 4511 outputs
-	//out PORTC, r16
 	numDisplayOut hundreds ; display hundreds
 	rcall delay
 	; switch to tens display
@@ -368,33 +286,28 @@ display:
 	andi working, 0b11110000
 	swap working
 	numDisplayOut working
-	//out PORTC, working ; display tens
 	rcall delay
 	; switch to ones Display
 	numDisplaySelect PB0
 	mov working, onesTens
 	andi working, 0b00001111
-	//out PORTC, working
 	numDisplayOut working
 	rcall delay ; admire
-	//rjmp reset ; restart the program to update input value and reset the required registers
 	ret ;
 
-scoreDisplay: // not working * 
-	scoreDisplaySelect PC5 ; select ones display
+scoreDisplay:
+	scoreDisplaySelect PC4 ; select ones display
 	mov r18, score ; retain score value
 	doubleDabble r18
 	mov working, onesTens
 	andi working, 0b00001111
 	shiftOut working
 	rcall delay
-	//rcall delay
-	scoreDisplaySelect PC4 ; select tens display
+	scoreDisplaySelect PC5 ; select tens display
 	mov working, onesTens
 	andi working, 0b11110000
 	swap working
 	shiftOut working
-	//rcall delay
 	mov r18, copy
 	doubleDabble r18
 	ret
@@ -409,32 +322,14 @@ L1: dec  r19
 	ret
 
 TIM2_COMPA:
-	//inc onesTens
-	//rcall checkEqual
-	//in original, PIND
-	//cp input, original
 	in input, PIND
-	//lpm original, z
-	cp input, copy //index
+	cp input, copy
 	breq isEqual
-	//breq isEqual
-	//mov hundreds, input
-
-	;rcall start
-
-	//rcall getInput
-	//clr input
-	//ldi original, 255
-	
-	//doubleDabble original
 	reti
 
 loop:
-	//rcall scoreDisplay
-	rcall display
+	rcall display ; display POV for challenge number and score
 	rcall scoreDisplay
-	//rcall scoreDisplay
-	//shiftData score
 	rjmp loop
 
 isEqual:
@@ -452,8 +347,6 @@ initShiftReg:
 	ret
 
 initScoreDisplay:
-	//ldi r18, 1 << PC4 | 1 << PC5
-	//out DDRC, r18
 	sbi DDRC, PC4
 	sbi DDRC, PC5
 	ret
@@ -467,5 +360,4 @@ initNumDisplay:
 	sbi DDRC, PC1
 	sbi DDRC, PC2
 	sbi DDRC, PC3
-
 	ret
